d285ca988121b8f4b754ae8457f619583cc92d13
Aibika <sabyraibika@gmail.com>
2024-12-19 17:49:00 +0600

    add validation to login and sign up forms

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,66 +1,126 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+'use client';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'Email is required.' })
+    .email('This is not a valid email.'),
+  password: z.string().min(1, { message: 'Password is required.' }),
+});
 
 export function LoginForm({
   className,
   ...props
-}: React.ComponentPropsWithoutRef<"div">) {
+}: React.ComponentPropsWithoutRef<'div'>) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardTitle className='text-2xl'>Login</CardTitle>
           <CardDescription>
             Enter your email below to login to your account
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                  autoComplete="username"
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className='flex flex-col gap-6'>
+                <div className='grid gap-2'>
+                  <FormField
+                    control={form.control}
+                    name='email'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor='email'>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            id='email'
+                            type='email'
+                            placeholder='m@example.com'
+                            autoComplete='username'
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 </div>
-                <Input id="password" type="password" required autoComplete="current-password"/>
+                <div className='grid gap-2'>
+                  <FormField
+                    control={form.control}
+                    name='password'
+                    render={({ field }) => (
+                      <FormItem>
+                        <div className='flex items-center'>
+                          <FormLabel htmlFor='password'>Password</FormLabel>
+                          <a
+                            href='#'
+                            className='ml-auto inline-block text-sm underline-offset-4 hover:underline'
+                          >
+                            Forgot your password?
+                          </a>
+                        </div>
+                        <FormControl>
+                          <Input
+                            id='password'
+                            type='password'
+                            autoComplete='current-password'
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button type='submit' className='w-full'>
+                  Login
+                </Button>
               </div>
-              <Button type="submit" className="w-full">
-                Login
-              </Button>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="signup" className="underline underline-offset-4">
+            </form>
+            <div className='mt-4 text-center text-sm'>
+              Don&apos;t have an account?{' '}
+              <a href='signup' className='underline underline-offset-4'>
                 Sign up
               </a>
             </div>
-          </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -1,18 +1,51 @@
+'use client';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { useForm } from 'react-hook-form';
+
+const formSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, { message: 'Email is required.' })
+      .email('This is not a valid email.'),
+    password: z.string().min(1, { message: 'Password is required.' }),
+    confirmPassword: z.string().min(1, { message: 'Confirm password.' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
 
 export function SignUpForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<'div'>) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
@@ -20,40 +53,84 @@ export function SignUpForm({
           <CardTitle className='text-2xl'>Sign up</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className='flex flex-col gap-6'>
-              <div className='grid gap-2'>
-                <Label htmlFor='email'>Email</Label>
-                <Input
-                  id='email'
-                  type='email'
-                  placeholder='m@example.com'
-                  required
-                  autoComplete='username'
-                />
-              </div>
-              <div className='grid gap-2'>
-                <div className='flex items-center'>
-                  <Label htmlFor='password'>Password</Label>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className='flex flex-col gap-6'>
+                <div className='grid gap-2'>
+                  <FormField
+                    control={form.control}
+                    name='email'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor='email'>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            id='email'
+                            type='email'
+                            placeholder='m@example.com'
+                            autoComplete='username'
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 </div>
-                <Input
-                  id='password'
-                  type='password'
-                  required
-                  autoComplete='current-password'
-                />
+                <div className='grid gap-2'>
+                  <FormField
+                    control={form.control}
+                    name='password'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor='password'>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            id='password'
+                            type='password'
+                            autoComplete='current-password'
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className='grid gap-2'>
+                  <FormField
+                    control={form.control}
+                    name='confirmPassword'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor='confirm-password'>
+                          Confirm Password
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            id='confirm-password'
+                            type='password'
+                            autoComplete='current-password'
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button type='submit' className='w-full'>
+                  Sign up
+                </Button>
               </div>
-              <Button type='submit' className='w-full'>
-                Sign up
-              </Button>
-            </div>
-            <div className='mt-4 text-center text-sm'>
-              Have an account?{' '}
-              <a href='login' className='underline underline-offset-4'>
-                Login
-              </a>
-            </div>
-          </form>
+              <div className='mt-4 text-center text-sm'>
+                Have an account?{' '}
+                <a href='login' className='underline underline-offset-4'>
+                  Login
+                </a>
+              </div>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
